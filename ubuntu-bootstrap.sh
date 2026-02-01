ccef3d281fa85cb0f99109c14b7ba6a5c02ca92d
ReevesA1 <adamrivard87@gmail.com>
2026-02-01 18:11:05 -0500

    d

--- ubuntu-bootstrap.sh
+++ ubuntu-bootstrap.sh
@@ -33,10 +33,22 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 
 echo
 echo "The following Flatpaks will be installed:"
-echo "  Flatpak 1: Mission Center"
-echo "  Flatpak 2: Flatseal"
-echo "  Flatpak 3: Warehouse"
-echo "  Flatpak 4: Flatsweep"
+echo "  Flatpak 1: Mail Viewer - EML and MSG file viewer"
+echo "  Flatpak 2: Fred TV - Fast And Powerful IPTV App"
+echo "  Flatpak 3: gThumb - Image Viewer"
+echo "  Flatpak 4: ZapZap - WhatsApp Messenger"
+echo "  Flatpak 5 - Ente Photos - Safe home for your photos"
+echo "  Flatpak 6 - PDF Arranger - Merge, shuffle, and crop PDFs"
+echo "  Flatpak 7 - Czkawka -  find duplicates, empty folders, similar images, broken files etc."
+echo "  Flatpak 8 - Musicfetch - Download songs and tag them"
+echo "  Flatpak 9 - Shortwave - Listen to internet radio"
+echo "  Flatpak 9 - Brasero - Create and copy CDs and DVDs"
+echo "  Flatpak 9 - Gapless - Play your music elegantly"
+echo "  Flatpak 9 - Impression - Create bootable drives"
+echo "  Flatpak 9 - Haruna - Media player"
+echo "  Flatpak 9 - LibreOffice - The LibreOffice productivity suite"
+echo "  Flatpak 9 - Extension Manager - Install GNOME Extensions - The LibreOffice productivity suite"
+echo "  Flatpak 9 - Gramps - perform genealogical research and analysis"
 echo
 
 printf "\033[33m👉 Install Adam's must-have Flatpaks? (y/N): \033[0m"
@@ -44,14 +56,22 @@ read INSTALL_FLATPAKS
 echo
 
 
-
-if [[ "$INSTALL_FLATPAKS" =~ ^([yY]|[yY][eE][sS])$ ]]; then
-  green_echo "Installing Adam's Must-Have Flatpaks..."
-
-  flatpak install -y flathub io.missioncenter.MissionCenter
-  flatpak install -y flathub com.github.tchx84.Flatseal
-  flatpak install -y flathub io.github.flattool.Warehouse
-  flatpak install -y flathub io.github.giantpinkrobots.flatsweep
+  flatpak install -y flathub io.github.alescdb.mailviewer
+  flatpak install flathub dev.fredol.open-tv
+  flatpak install flathub org.gnome.gThumb
+  flatpak install flathub com.rtosta.zapzap
+  flatpak install flathub io.ente.photos
+  flatpak install flathub com.github.jeromerobert.pdfarranger
+  flatpak install flathub com.github.qarmin.czkawka
+  flatpak install flathub net.fhannenheim.musicfetch
+  flatpak install flathub de.haeckerfelix.Shortwave
+  flatpak install flathub org.gnome.Brasero
+  flatpak install flathub com.github.neithern.g4music
+  flatpak install flathub io.gitlab.adhami3310.Impression
+  flatpak install flathub org.kde.haruna
+  flatpak install flathub org.libreoffice.LibreOffice
+  flatpak install flathub com.mattjakeman.ExtensionManager
+  flatpak install flathub org.gramps_project.Gramps
 
 
 else
