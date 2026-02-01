8797ab262eada27f6a68c18323c1648b174b5809
ReevesA1 <adamrivard87@gmail.com>
2026-02-01 14:37:24 -0500

    s

--- ubuntu-bootstrap.sh
+++ ubuntu-bootstrap.sh
@@ -1,20 +1,30 @@
 #!/usr/bin/env bash
 set -e
 
+
+#!#########################     Variables      ######################################
+
 # Green text helper + pause
 green_echo() {
   echo -e "\e[32m$1\e[0m"
   sleep 3
 }
 
+#!#########################     Update      ######################################
+green_echo "Hi Rick I made this interactive script for you to try"
+
 green_echo "Updating system..."
 sudo apt update && sudo apt upgrade -y
+
+##########################     Ubuntu Dock      ######################################
 
 green_echo "Fixing Ubuntu Dock..."
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
 gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 
+
+#!#########################     Flatpaks       ######################################
 green_echo "Installing Flatpak Support..."
 sudo apt install -y flatpak
 
@@ -22,22 +32,28 @@ green_echo "Adding Flathub repository..."
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo
+echo "The following Flatpaks will be installed:"
+echo "  Flatpak 1: Mission Center"
+echo "  Flatpak 2: Flatseal"
+echo "  Flatpak 3: GNOME Calculator"
+echo
+
 printf "\033[33m👉 Install Adam's must-have Flatpaks? (y/N): \033[0m"
 read INSTALL_FLATPAKS
 echo
 
 
+
 if [[ "$INSTALL_FLATPAKS" =~ ^([yY]|[yY][eE][sS])$ ]]; then
   green_echo "Installing Adam's Must-Have Flatpaks..."
 
-  green_echo "Flatpak 1: Mission Center"
   flatpak install -y flathub io.missioncenter.MissionCenter
 
-  green_echo "Flatpak 2: PLACEHOLDER"
-  # flatpak install -y flathub com.example.App
 
 else
   green_echo "Skipping Flatpak installs."
 fi
+
+#!#########################     Done       ######################################
 
 green_echo "Done! You may need to log out and log back in for GNOME changes to fully apply."
